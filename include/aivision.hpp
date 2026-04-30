e1e73f3d9fa64516e06b21e03c2dabb5b45b8cc7
ZayK12 <27kazi-zayyaan@jonesboroschools.net>
2026-04-30 10:09:20 -0500

    Further refactor of aivision.hpp
    Made everything more explicit on floats, removed a namespace and put under class, optimized initial math

--- include/aivision.hpp
+++ include/aivision.hpp
@@ -1,6 +1,7 @@
 #include "api.h"
 #include <cmath>
-
+#include <numbers>
+#define PI std::numbers::pi_v<float> // M_PI from cmath is a double, float on float operations prevent casting
 class AIVisionLocalizer {
     public:
         struct Pose {
@@ -10,9 +11,8 @@ class AIVisionLocalizer {
             float theta = 0.0;
         };
         float tag_width = 1.2606; // turns out the pixel derivation was correct
-        // float tolerance_percent = 0.9; // Detections below 90% are disregarded, as recommended from Jhon @ ATUM.
 
-        AIVisionLocalizer(pros::AIVision& _VisionSensor/*, float tolerance=0.9*/);
+        AIVisionLocalizer(pros::AIVision& _VisionSensor);
         
         std::vector<AIVisionLocalizer::Pose> getPositionsRelative(const int tag); // Pose{dx, dy, r, theta}
         std::optional<AIVisionLocalizer::Pose> getNearestRelative(const int tag); // Pose{dx, dy, r, theta}
@@ -20,17 +20,21 @@ class AIVisionLocalizer {
         std::vector<AIVisionLocalizer::Pose> getPositionsGlobal(const int tag, std::array<float, 3> robot_pose); // Pose{x, y, r, theta}
         std::optional<AIVisionLocalizer::Pose> getNearestGlobal(const int tag, std::array<float, 3> robot_pose); // Pose{x, y, r, theta}
     private:
+        float AIVisionLocalizer::side(float x1, float y1, float x2, float y2); // no need for this to be public -ZK
+
         // AI Vision Sensor constants. These were derived from:
         // https://kb.vex.com/hc/en-us/articles/24173352365972-Comparing-the-AI-Vision-Sensor-to-the-V5-Vision-Sensor
-        inline static constexpr float horizontal_fov_deg = 74;
+        inline static constexpr float horizontal_fov_deg = 74; 
         inline static constexpr float vertical_fov_deg = 63;
         inline static constexpr float diagonal_fov_deg = 87;
         inline static constexpr float resolution_x = 320;
         inline static constexpr float resolution_y = 240;
+        // No need for these to be converted into ints they are compiled immeditely below -ZK
 
         // Constants created for projection
-        inline static const float focal_length = (resolution_x / 2) / tan(horizontal_fov_deg * M_PI / 180.0f / 2.0f);
-        inline static const float half_res_x = (resolution_x / 2);
+        inline static const float focal_length = (resolution_x * 0.5f) / tan(horizontal_fov_deg * PI / 360.0f); // (PI/180) / 2 = (PI/360) but less div operations. -ZK
+        inline static const float half_res_x = (resolution_x * 0.5f); // multiplying by .5 is faster than dividing by 2 - ZK
+        float tolerance = .9; // UNUSED  Detections below 90% are disregarded, as recommended from Jhon @ ATUM.
         
         // Main AI Sensor
         pros::AIVision& VisionSensor;
@@ -38,7 +42,7 @@ class AIVisionLocalizer {
         Pose project(const pros::aivision_object_tag_s_t& tag);
 };
 
-AIVisionLocalizer::AIVisionLocalizer(pros::AIVision& _VisionSensor/*, float tolerance=0.9*/)
+AIVisionLocalizer::AIVisionLocalizer(pros::AIVision& _VisionSensor)
      : VisionSensor(_VisionSensor) {
     VisionSensor.disable_detection_types(pros::AivisionModeType::all); // disable 
     VisionSensor.enable_detection_types(pros::AivisionModeType::tags);
@@ -56,13 +60,14 @@ Mathematics for April Tag usage (based on 4 corners):
     x = ( centroid_x - image_cx ) * y / focal_length
 
 */
-namespace{
-    float side(float x1, float y1, float x2, float y2) {
-        float dX = x2 - x1;
-        float dY = y2 - y1;
-        return std::sqrtf(dX * dX + dY * dY);
-    }
+
+//Moved to member function -ZK 4/30/26
+float AIVisionLocalizer::side(float x1, float y1, float x2, float y2) { 
+    float dX = x2 - x1;
+    float dY = y2 - y1;
+    return std::sqrtf(dX * dX + dY * dY);
 }
+
 
 AIVisionLocalizer::Pose AIVisionLocalizer::project(const pros::aivision_object_tag_s_t& tag) {
     Pose testpose;
@@ -100,9 +105,10 @@ AIVisionLocalizer::get_nearest essentially derives the area of each tag (in pixe
 Logically, the tag with the largest area should be default also be the closest to the camera.
 */
 
+//moved all to floats as precision isn't needed -ZK 4/30/26
 std::optional<AIVisionLocalizer::Pose> AIVisionLocalizer::getNearestRelative(const int tag) {
     std::optional<pros::aivision_object_tag_s_t> best = std::nullopt;
-    double best_area = 0;
+    float best_area = 0;
  
     const int count = VisionSensor.get_object_count();
     if (count <= 0) return std::nullopt;
@@ -113,12 +119,12 @@ std::optional<AIVisionLocalizer::Pose> AIVisionLocalizer::getNearestRelative(con
         if (object.id != tag || !pros::AIVision::is_type(object, pros::AivisionDetectType::tag)) continue;
  
         // Shoelace formula for quadrilateral areas
-        const double area = 0.5 * std::abs(
-            static_cast<double>(object.object.tag.x0) * (object.object.tag.y1 - object.object.tag.y3) +
-            static_cast<double>(object.object.tag.x1) * (object.object.tag.y2 - object.object.tag.y0) +
-            static_cast<double>(object.object.tag.x2) * (object.object.tag.y3 - object.object.tag.y1) +
-            static_cast<double>(object.object.tag.x3) * (object.object.tag.y0 - object.object.tag.y2)
-        );
+        const float area = 0.5 * std::abs(
+            (object.object.tag.x0) * (object.object.tag.y1 - object.object.tag.y3) +
+            (object.object.tag.x1) * (object.object.tag.y2 - object.object.tag.y0) +
+            (object.object.tag.x2) * (object.object.tag.y3 - object.object.tag.y1) +
+            (object.object.tag.x3) * (object.object.tag.y0 - object.object.tag.y2)
+        ); //Ints are immedietly converted to floats with 0 runtime, conversionsn aren't needed. -ZK
         if (area >= best_area) {
             best_area = area;
             best = object.object.tag;
@@ -140,9 +146,9 @@ std::vector<AIVisionLocalizer::Pose> AIVisionLocalizer::getPositionsGlobal(const
     for (const Pose& relative : relative_poses) {
         Pose global;
         global.r = relative.r;
-        global.theta = std::fmod(relative.theta + robot_pose[2], 2 * M_PI);
-        global.x = robot_pose[0] + relative.r * std::cos(global.theta);
-        global.y = robot_pose[1] + relative.r * std::sin(global.theta);
+        global.theta = std::fmod(relative.theta + robot_pose[2], 2 * PI);
+        global.x = robot_pose[0] + relative.r * std::cosf(global.theta);
+        global.y = robot_pose[1] + relative.r * std::sinf(global.theta);
         global_poses.push_back(global);
     }
     return global_poses;
@@ -154,8 +160,8 @@ std::optional<AIVisionLocalizer::Pose> AIVisionLocalizer::getNearestGlobal(const
 
     Pose global;
     global.r = relative->r;
-    global.theta = std::fmod(relative->theta + robot_pose[2],2 * M_PI);
-    global.x = robot_pose[0] + relative->r * std::cos(global.theta);
-    global.y = robot_pose[1] + relative->r * std::sin(global.theta);
+    global.theta = std::fmod(relative->theta + robot_pose[2],2 * PI);
+    global.x = robot_pose[0] + relative->r * std::cosf(global.theta);
+    global.y = robot_pose[1] + relative->r * std::sinf(global.theta);
     return std::optional<Pose>(global);
 }
